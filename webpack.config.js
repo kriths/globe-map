500e96c3c40c8ea1b895f8f8512a6b46e244ff5e
Thomas Krisch <thomas.krisch+git@gmail.com>
2021-09-10 22:50:43 +0200

    chore: enable TypeScript for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
@@ -14,5 +23,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 };
